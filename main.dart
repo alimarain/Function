77d7e83978c481812876117c02fdafb2b66b3bbd
alimuhammad <alimarain55@gmail.com>
2025-05-11 12:54:45 +0500

    this is function code

---
+++ main.dart
@@ -0,0 +1,17 @@
+void main(){
+  print("--------------------------------");
+  print("|    This is function code!    |");
+  print("--------------------------------");
+
+  printMyNameandAge(){
+    print("My name is Ali Muhammad");
+    print("My age is 30");
+  }
+    addTwoNumber(){
+    int a = 16;
+    int b = 44;
+    print("sum of $a and $b is ${a+b}");
+  }
+  printMyNameandAge();
+  addTwoNumber();
+}
