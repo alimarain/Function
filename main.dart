9beb434011884701e7ffc6433c665bbd99a2805f
alimuhammad <alimarain55@gmail.com>
2025-05-11 13:33:03 +0500

    passing parameters

--- main.dart
+++ main.dart
@@ -7,14 +7,22 @@ void main(){
   //   print("My name is Ali Muhammad");
   //   print("My age is 30");
 
-    int calculateTotalMarks(){
-    int a = 16;
-    int b = 44;
-    int c = 23;
+//     int calculateTotalMarks(){
+//     int a = 16;
+//     int b = 44;
+//     int c = 23;
+//     int totalMarks = a + b + c;
+//     return totalMarks;  
+    
+//   }
+// int totalMarks = calculateTotalMarks();
+//   print(totalMarks);
+    int calculateTotalMarks(int a, int b, int c){
+   
     int totalMarks = a + b + c;
     return totalMarks;  
     
   }
-int totalMarks = calculateTotalMarks();
+int totalMarks = calculateTotalMarks(45,95,35);
   print(totalMarks);
 }
